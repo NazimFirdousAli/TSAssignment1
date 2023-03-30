46681fc5398985b3a7e911663293e45be9389993
NazimFirdousAli <51528955+NazimFirdousAli@users.noreply.github.com>
2023-03-30 17:50:56 +0500

    update

--- dist/index.js
+++ dist/index.js
@@ -9,18 +9,18 @@ const options = {
 fetch('https://car-data.p.rapidapi.com/cars?limit=10&page=0', options)
     .then(response => response.json())
     .then(data => {
-    const tableBody = document.querySelector('#data');
-    data.forEach((item) => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
+        const tableBody = document.querySelector('#data');
+        data.forEach((item) => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
         <td>${item.id}</td>
         <td>${item.make}</td>
         <td>${item.model}</td>
         <td>${item.type}</td>
         <td>${item.year}</td>
       `;
-        tableBody === null || tableBody === void 0 ? void 0 : tableBody.appendChild(row);
-    });
-})
+            tableBody.appendChild(row);
+        });
+    })
     .catch(error => console.log(error));
 //# sourceMappingURL=index.js.map
